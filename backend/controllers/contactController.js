dd7bbdc29f5040bdec85631260dc4b6a34a10287
Akshay <98738875+finiky@users.noreply.github.com>
2023-08-02 00:16:00 +0530

    added errorHandler.js for handling diferent error types

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -2,7 +2,14 @@ const getContacts = (request, response) => {
   response.status(200).json({ message: "Get all contacts" });
 };
 const createContact = (request, response) => {
-  response.status(201).json({ message: "Create a contact" });
+  const { name, email, phone } = request.body;
+  if (!name || !email || !phone) {
+    response.status(400);
+    throw new Error(
+      "Kindly ensure that the name, email and phone fields are filled."
+    );
+  }
+  response.status(201).json({ name: name, email: email, phone: phone });
 };
 const getContact = (request, response) => {
   response.status(200).json({ message: "Get a single contact" });
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 const express = require("express");
-const dotenv = require("dotenv").config()
+const dotenv = require("dotenv").config();
 const contactsRouter = require("./routes/contacts.router");
-const app = express();
+const errorHandling = require("./middleware/errorHandler");
 
 const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(express.json());
 app.use("/", contactsRouter);
+app.use(errorHandling);
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}.`);
 });
