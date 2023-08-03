883fcc91b4c1ccab4de3594d82a1cc0de122f9a6
Akshay <98738875+finiky@users.noreply.github.com>
2023-08-04 02:08:00 +0530

    CSS for the Login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import { Route, Routes } from "react-router-dom";
+import styles from "./App.module.css";
 import Login from "./Login";
 
 const App = () => {
   return (
-    <div>
-      <h1>Contact Manager</h1>
+    <div className={styles.main}>
+      <h1 className={styles.heading}>Contact Manager</h1>
       <Routes>
         <Route exact path="/login" element={<Login />} />
       </Routes>
--- client/src/Login.js
+++ client/src/Login.js
@@ -5,31 +5,37 @@ const Login = () => {
   const [password, setPassword] = useState("");
   return (
     <div className={styles.main}>
-      Sign In
-      <label htmlFor="email">
-        Email Id:{" "}
-        <input
-          id="email"
-          className={styles.input}
-          placeholder="Email Id"
-          value={email}
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
-        />
-      </label>
-      <label>
-        Password:{" "}
-        <input
-          id="password"
-          className={styles.input}
-          placeholder="Password"
-          value={password}
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-        />
-      </label>
+      <form className={styles.form}>
+        <div className={styles.inputDiv}>
+          <label htmlFor="email">Email Id</label>
+          <input
+            id="email"
+            className={styles.input}
+            type="email"
+            placeholder="Email Id"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+        </div>
+        <div className={styles.inputDiv}>
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            className={styles.input}
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+        </div>
+        <button className={styles.button} type="submit">
+          Sign In
+        </button>
+      </form>
     </div>
   );
 };
