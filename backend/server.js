c1f137abed42fe0df99e57f0491a37671d2725f9
Akshay <98738875+finiky@users.noreply.github.com>
2023-08-04 03:05:59 +0530

    installed cors on backend and included it in server.js  changed the /api/users/login to POST method

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const dotenv = require("dotenv").config();
 const contactsRouter = require("./routes/contacts.router");
 const userRouter = require("./routes/users.router");
@@ -7,7 +8,7 @@ const connectDb = require("./config/dbConnection");
 const port = process.env.PORT || 5000;
 const app = express();
 connectDb();
-
+app.use(cors());
 app.use(express.json());
 app.use("/", contactsRouter);
 app.use("/", userRouter);
