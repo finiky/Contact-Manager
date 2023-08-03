9a6c1883cdefc8d3f0da98c48c1abde135e761da
Akshay <98738875+finiky@users.noreply.github.com>
2023-08-04 03:07:53 +0530

    Login changed to POSt method, added required attribute in email and password input field

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,18 +1,33 @@
 import { useState } from "react";
+// import { useNavigate } from "react-router-dom";
 import styles from "./Login.module.css";
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  //   const navigate = useNavigate();
+  const handleSubmit = async () => {
+    const response = await fetch("http://localhost:5002/api/users/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    });
+    if (response.status === 200) {
+      console.log("Login Success");
+    } else {
+      console.log("Login Unsuccessful");
+    }
+  };
   return (
     <div className={styles.main}>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={() => handleSubmit()}>
         <div className={styles.inputDiv}>
           <label htmlFor="email">Email Id</label>
           <input
             id="email"
             className={styles.input}
             type="email"
-            placeholder="Email Id"
+            placeholder="email id"
+            required
             value={email}
             onChange={(e) => {
               setEmail(e.target.value);
@@ -25,7 +40,8 @@ const Login = () => {
             id="password"
             className={styles.input}
             type="password"
-            placeholder="Password"
+            placeholder="password"
+            required
             value={password}
             onChange={(e) => {
               setPassword(e.target.value);
