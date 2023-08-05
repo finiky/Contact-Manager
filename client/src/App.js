c501cf0f6659640cf92f1f56fbd4a0eb1b563ad0
Akshay <98738875+finiky@users.noreply.github.com>
2023-08-05 06:49:19 +0530

    Register component creation and addded a route for it in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import styles from "./App.module.css";
 import Login from "./Login";
 import Contacts from "./Contacts";
 import Contact from "./Contact";
+import Register from "./Register";
 
 const App = () => {
   return (
     <div className={styles.main}>
       <h1 className={styles.heading}>Contact Manager</h1>
       <Routes>
+        <Route exact path="/register" elememt={<Register />} />
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/contacts" element={<Contacts />} />
         <Route exact path="/contacts/:contactid" element={<Contact />} />
